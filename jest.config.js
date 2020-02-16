569f0c72704bb8cf82aec29c067bf3933bc57762
Souad <Souad@polarstork.com>
2020-02-16 23:31:29 +0200

    Coverage Report

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupTestFrameworkScriptFile: '<rootDir>/src/setup-jest.ts',
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{js,jsx,ts}"],
+  collectCoverageFrom: [
+      "src/**/*.{js,jsx,ts,tsx}",
+      "!<rootDir>/node_modules/"
+  ],
   moduleNameMapper: {
     '@core/(.*)': '<rootDir>/src/app/core/$1',
     '@state/(.*)': '<rootDir>/src/app/state/$1'
